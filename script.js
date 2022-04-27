4eeb7433d90d7fdf5e46ac055b5bb2c005293a5e
gagalebig <gaetan.fontaine2006@gmail.com>
2022-04-28 00:40:03 +0200

    RETOUR EN ARRIERE
    après un mois, bug trouvé
    un inversement dans attack (vers overlap) de attack et attack2
    
    POSSIBILITE QUE LE PYTHON NE FASSE PAS FONCTIONNER LES REACTIONS AUX SAUTS
    
    URGENTE VERIFICATION
    STOP MODIF AVANT TROUVER SOLUTION POTABLE !!!

--- script.js
+++ script.js
@@ -1,35 +1,33 @@
-
+// test
 var perso;
 var perso_walk;
 var perso_wait;
 var mur = [];
 var test = 10;
-
 function preload() {
   perso_sprite_sheet_walk = loadSpriteSheet('asset/sonic_1.png', 48, 48, 6);
   perso_sprite_sheet_wait = loadSpriteSheet('asset/sonic_2.png', 48, 48, 4);
 }
-
 function setup() {
   X = 1346; // window.innerWidth-20;
   Y = 605; // window.innerHeight-20;
   x = X/2;
   y = Y/2;
   createCanvas(X,Y);
-
   perso=createSprite(2*X/3,y,48,48);
   perso.velocity.y = -1;
   perso.jump = 0;
   perso.prevjump = millis();
   perso.prevattack = -1000;
   perso.orientation = 1;
-
   perso2=createSprite(X/3,y,48,48);
   perso2.velocity.y = -1;
   perso2.jump = 0;
   perso2.prevjump = millis();
   perso2.prevattack = -1000;
   perso2.orientation = 1;
+
+
 
   attack = createSprite(perso.position.x,perso.position.y,100,5);
   attack2 = createSprite(perso2.position.x,perso2.position.y,100,5);
@@ -46,7 +44,7 @@ function setup() {
   murDroite.immovable = true;
   murHaut.immovable = true;
   murBas.immovable = true;
-
+  
   perso_walk=loadAnimation(perso_sprite_sheet_walk);
   perso_wait=loadAnimation(perso_sprite_sheet_wait);
   perso_walk.frameDelay=4;
@@ -54,7 +52,6 @@ function setup() {
   perso.addAnimation("walk",perso_walk);
   perso.addAnimation("wait",perso_wait);
   perso.changeAnimation("wait");
-
   perso2_walk=loadAnimation(perso_sprite_sheet_walk);
   perso2_wait=loadAnimation(perso_sprite_sheet_wait);
   perso2_walk.frameDelay=4;
@@ -62,22 +59,20 @@ function setup() {
   perso2.addAnimation("walk",perso_walk);
   perso2.addAnimation("wait",perso_wait);
   perso2.changeAnimation("wait");
-
 }
 function draw() {
   background(240);
   perso.addSpeed(0.2, 90);
   perso2.addSpeed(0.2, 90);
   collisions();
-
   
-
   if (perso.prevattack+150 < millis()){
-//attack.remove();
+    // attack.remove();
   }
-if (perso2.prevattack+150 < millis()){
-//attack2.remove();
+  if (perso2.prevattack2+150 < millis()){
+    // attack.remove();
   }
+
   if(keyIsDown(37) && perso.position.x >= -5+30+test/2) {perso.position.x-=3; perso.mirrorX(-1); perso.orientation=-1;}
   if(keyIsDown(39) && perso.position.x <= X+5-30-test/2) {perso.position.x+=3; perso.mirrorX(1); perso.orientation=1;}
   if(keyIsDown(40)) {y-=0}
@@ -91,15 +86,9 @@ if (perso2.prevattack+150 < millis()){
   if(! (keyIsDown(37) || keyIsDown(39) || keyIsDown(38) || keyIsDown(40))){
     perso.changeAnimation("wait");
   }
-
   if(keyIsDown(223)){
     perso.prevattack = millis();
   }
-
-  if(keyIsDown(65)){
-    perso2.prevattack = millis();
-  }
-
   if(perso.prevattack+10 > millis()){
     attack.position.x = perso.position.x + 80*perso.orientation;
     attack.position.y = perso.position.y;
@@ -108,13 +97,6 @@ if (perso2.prevattack+150 < millis()){
     attack.position.y = -500;
   }
 
- if(perso2.prevattack+10 > millis()){
-    attack2.position.x = perso2.position.x + 80*perso2.orientation;
-    attack2.position.y = perso2.position.y;
-  } else {
-    attack2.position.x = -500;
-    attack2.position.y = -500;
-  }
   if(keyIsDown(81) && perso2.position.x >= -5+30+test/2) {perso2.position.x-=3; perso2.mirrorX(-1); perso2.orientation=-1;}
   if(keyIsDown(68) && perso2.position.x <= X+5-30-test/2) {perso2.position.x+=3; perso2.mirrorX(1); perso2.orientation=1;}
   if(keyIsDown(83)) {y-=0}
@@ -128,13 +110,22 @@ if (perso2.prevattack+150 < millis()){
   if(! (keyIsDown(81) || keyIsDown(68) || keyIsDown(90) || keyIsDown(83))){
     perso2.changeAnimation("wait");
   }
+  if(keyIsDown(65)){
+    perso2.prevattack = millis();
+  }
+
+  if(perso2.prevattack+10 > millis()){
+    attack2.position.x = perso2.position.x + 80*perso2.orientation;
+    attack2.position.y = perso2.position.y;
+  } else {
+    attack2.position.x = -500;
+    attack2.position.y = -500;
+  }
 
 
   drawSprites();
   dev();
 }
-
-
 function collisions(){
   murGauche.touchej1 = false;
   murDroite.touchej1 = false;
@@ -148,16 +139,15 @@ function collisions(){
   perso.bounce(murDroite,toucherDroitej1);
   perso.bounce(murHaut,toucherHautj1);
   perso.bounce(murBas,toucherBasj1);
-  perso.overlap(attack,toucherAttackj1)
+  perso.overlap(attack2,toucherAttackj1)
   perso2.bounce(murGauche,toucherGauchej2);
   perso2.bounce(murDroite,toucherDroitej2);
   perso2.bounce(murHaut,toucherHautj2);
   perso2.bounce(murBas,toucherBasj2);
-  perso2.overlap(attack2,toucherAttackj2)
+  perso2.overlap(attack,toucherAttackj2)
 }
 
 //--------------------- Perso 1 ---------------------
-
 function toucherBasj1(){
   murBas.touchej1 = true;
   perso.velocity.y = 0;
@@ -177,6 +167,7 @@ function toucherAttackj1(){
   perso.velocity.x = 0;
   perso.velocity.y = 0;
   perso.addSpeed(10,270);
+
 }
 
 //--------------------- Perso 2 ---------------------
@@ -194,18 +185,15 @@ function toucherDroitej2(){
 }
 function toucherGauchej2(){
   murGauche.touchej2 = true;
+;
 }
 function toucherAttackj2(){
   perso2.jump = 1;
   perso2.velocity.x = 0;
   perso2.velocity.y = 0;
   perso2.addSpeed(10,270);
-
 }
-
-
-//--------------------  DEV  -------------------------------
-
+//---------------------------------------------------
 function dev() {
   text(mouseX +" "+ mouseY,mouseX,mouseY);
   text("x: "+floor(perso.position.x) +"\ny: "+ floor(perso.position.y),perso.position.x-20,perso.position.y);
@@ -214,6 +202,5 @@ function dev() {
       +"\nmillis: "+floor(millis())
       +"\njump: "+perso.jump
       +"\nattack.x: "+floor(attack.position.x)
-
     ,test,test+10)
 }
